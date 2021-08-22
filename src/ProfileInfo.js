40791eae8f3e4abe03af3a4955e79a887b7cb599
Jusepa <andrey.ru754@gmail.com>
2021-08-22 03:08:27 +0300

    feat: add ProfileInfo element

---
+++ src/ProfileInfo.js
@@ -0,0 +1,51 @@
+import React, {useState} from "react";
+import "./ProfileInfo.scss"
+import MoreHorizIcon from "@material-ui/icons/MoreHoriz";
+import {Menu, Popover, Avatar} from '@material-ui/core';
+import MenuItem from '@material-ui/core/MenuItem';
+
+function ProfileInfo({displayName, username, avatar, onClick}) {
+
+    const [anchorEl, setAnchorEl] = useState(null);
+
+    const handleClick = (event) => {
+        setAnchorEl(event.currentTarget);
+    };
+    
+    const handleClose = () => {
+        setAnchorEl(null);
+    };
+
+    return (
+        <div>
+            <div id="box" className="box" onClick={handleClick}>
+                <Avatar src={avatar}/>
+                <div className="box__info" >
+                    <span id="displayname">{displayName}</span>
+                    <span id="username">{username}</span>
+                </div>
+                <div className="box__more">
+                    <MoreHorizIcon />
+                </div>
+            </div>
+            <Menu
+                anchorEl={anchorEl}
+                anchorOrigin={{
+                    vertical: 'top',
+                    horizontal: 'center',
+                  }}
+                transformOrigin={{
+                    vertical: 'bottom',
+                    horizontal: 'center',
+                }}
+                keepMounted
+                open={Boolean(anchorEl)}
+                onClose={handleClose}
+            >
+                <MenuItem onClick={handleClose}>Sign out</MenuItem>
+            </Menu>
+        </div>
+    );
+}
+
+export default ProfileInfo;
